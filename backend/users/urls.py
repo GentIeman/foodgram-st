ac18a9b4209b810cb1756517911e956d0e3797e4
ilyas <ilyashepelev23@gmail.com>
2025-05-07 00:50:17 +0300

    fix(urls): add trailing slash configuration to DefaultRouter

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,11 +2,11 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=True)
 router.register('users', views.UserViewSet, basename='users')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('', include(router.urls)),
 ]
