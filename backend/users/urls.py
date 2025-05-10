7f2665fcebc7ed837498c99ebb790b59c5d02513
ilyas <ilyashepelev23@gmail.com>
2025-05-10 12:41:06 +0300

    refactor(users): simplify router configuration by removing trailing slash and unnecessary imports

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,11 +2,10 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter(trailing_slash=True)
-router.register('users', views.UserViewSet, basename='users')
+router = DefaultRouter()
+router.register('users', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
