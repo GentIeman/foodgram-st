3d8144951533f1a9f25dc67c58fea22c8e7f98f3
ilyas <ilyashepelev23@gmail.com>
2025-05-24 15:40:03 +0300

    feat(urls): remove DjoserUserViewSet password reset endpoint

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from . import views
-from djoser.views import UserViewSet as DjoserUserViewSet
 
 router = DefaultRouter()
 router.register('users', views.UserViewSet, basename='users')
@@ -12,7 +11,6 @@ urlpatterns = [
         'post': 'set_avatar',
         'delete': 'delete_avatar'
     })),
-    path('users/set_password/', DjoserUserViewSet.as_view({'post': 'set_password'})),
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
