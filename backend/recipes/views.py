265c2f17c2d28f23a1e6fe320e6da7c596e516f2
ilyas <ilyashepelev23@gmail.com>
2025-05-24 15:40:20 +0300

    feat(views): remove max_page_size from custom pagination

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -39,7 +39,6 @@ class CustomPagination(PageNumberPagination):
     """Пагинация с настраиваемым размером страницы"""
     page_size = 6
     page_size_query_param = 'limit'
-    max_page_size = 100
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
